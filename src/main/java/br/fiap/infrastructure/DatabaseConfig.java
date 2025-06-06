ff50ae455e3fb18eb91b8acbca2ee8d141a66114
felipecarlos1 <fca.felipe1@gmail.com>
2025-06-06 09:42:33 -0300

    Update database credentials in DatabaseConfig

--- src/main/java/br/fiap/infrastructure/DatabaseConfig.java
+++ src/main/java/br/fiap/infrastructure/DatabaseConfig.java
@@ -7,8 +7,8 @@ public class DatabaseConfig {
 
     public static final String URL =
             "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL";
-    public static final String USER = "rm560601";
-    public static final String PASSWORD = "040301";
+    public static final String USER = "rm560928";
+    public static final String PASSWORD = "180305";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
